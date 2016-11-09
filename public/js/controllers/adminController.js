4e938458b19ed9fa73d5c50a45460f4418e32ef7
Milan Bojic <mbojic@domain.local>
2016-11-09 11:25:40 +0100

    survey 0.21

--- public/js/controllers/adminController.js
+++ public/js/controllers/adminController.js
@@ -47,6 +47,7 @@ mApp.controller('adminController',function($scope, $location ,surveyService)
 	
 	$scope.runSurvey = function()
 	{
+		if($scope.question != "") return alert("Novi pitanje nije snimljeno. Snimite ga ili obrišite njegov sadržaj!");
 		if(!$scope.surveyName) return alert("Morate unjeti naziv ankete!");
 		if($scope.rows.length === 0 )return alert("Anketa mora imati makar jedno pitanje!");
 		//console.log("test");
